fadb4357aa8c79f65b92e229e91d9bbac7ac0cec
suheyla-turan <159693059+suheyla-turan@users.noreply.github.com>
2026-05-24 16:43:49 +0300

    Delete kod 2 directory

--- kod 2/calistir-konsol.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-if not exist bin mkdir bin
-javac -encoding UTF-8 -d bin otomata_teorisi.java
-if errorlevel 1 pause & exit /b 1
-java -Dfile.encoding=UTF-8 -cp bin otomata_teorisi --konsol
-pause
